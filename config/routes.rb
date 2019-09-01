2448988f1c106f50a4bde2f5fa14811904757aed
kang sheng <taykangsheng@spgroup.com.sg>
2019-09-01 16:56:57 +0800

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users
+
+  get "/signin", to: "users/sessions#new", as: "signin"
+  post "/signin", to: "users/sessions#create"
+  delete "/signout", to: "users/sessions#destroy", as: "signout"
 end
